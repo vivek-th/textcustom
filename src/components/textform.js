59eee35bcd5a29eb5e81688e969c3354060ee10d
vivek_thakur <vivekthakuruspc@gmail.com>
2023-08-08 23:53:00 +0530

    add lowercase button , word and character count and preview window

--- src/components/textform.js
+++ src/components/textform.js
@@ -11,6 +11,11 @@ export default function Textform(props) {
         console.log("on change");
         setText(event.target.value);
     }
+    const handleLoClick=()=>{
+      console.log("convert to lowercase");
+      let newText=text.toLowerCase();
+      setText(newText)
+    }
   const [text, setText] = useState("Enter text here");
   return (
     <>
@@ -19,11 +24,14 @@ export default function Textform(props) {
       <div className="mb-3">
         <textarea className="form-control" value={text}  onChange={handleOnChange} id="exampleFormControlTextarea1" rows="8"> </textarea>
       </div>
-      <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+      <button className="btn btn-primary mx-3" onClick={handleUpClick}>Convert to Uppercase</button>
+      <button className="btn btn-primary mx-3" onClick={handleLoClick}>Convert to Lowercase</button>
     </div>
     
     <div className="container">
-        <p>{text.split(" ").length} words and {text.length} characters in your paragraph</p>
+        <p className="my-3">{text.split(" ").length} words and {text.length} characters in your paragraph</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
     </div>
     </>
   
